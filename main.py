36c8df606cae779160f8d0ba353406f6ff57cc06
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-09-17 17:18:37 +0300

    add move and rotate functions to player

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from constants import *
+from player import Player
 import pygame
+
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip() # display update
+
         dt = clock.tick(60)/1000 # limit the frame rate and store the delta time in seconds
+
+
 if __name__ == "__main__":
     main()
