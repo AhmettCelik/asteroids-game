59bc8d5ec9d1730b1c5ba8992c3a4dccd52dc5b2
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-09-17 16:01:50 +0300

    delta time

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,5 +15,6 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip() # display update
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
