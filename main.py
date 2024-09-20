a32519d3b53dfc7f275d165fad75f8b738c4b8f3
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-09-20 16:57:37 +0300

    asteroid and bullet collision

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+
+            for bullet in shots:
+                if asteroid.collision(bullet):
+                    asteroid.kill()
+                    bullet.kill()
         
         screen.fill("black")
         
